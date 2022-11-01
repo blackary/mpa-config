0a64a03cd32539ac07f78ae78ea774750ee84926
Zachary Blackwood <zachary@streamlit.io>
2022-11-01 15:43:21 -0400

    Use button

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,19 @@ from page_show import show_pages
 
 """# Dynamically Show and Hide Pages in MPA"""
 
+
 with st.echo("above"):
-    st.checkbox("Simulate logged-in user", key="logged_in")
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    if st.session_state.logged_in:
+        if st.button("Log out"):
+            st.session_state.logged_in = False
+            st.experimental_rerun()
+    else:
+        if st.button("Log in"):
+            st.session_state.logged_in = True
+            st.experimental_rerun()
 
     if st.session_state["logged_in"]:
         show_pages(
@@ -21,8 +32,8 @@ with st.echo("above"):
         show_pages(["streamlit_app"])
 
 """
-Note that this checkbox doesn't just hide the sidebar, it actually removes the
-pages from the page list. To see this, uncheck the checkbox and then
+Note that these log out / log in buttons don't just hide the sidebar, it actually
+removes the pages from the page list. To see this, log out and then
 click on this link to try to visit [/example_one](/example_one). You'll get a
 `Page Not Found` error
 """
